07d14dcb7651e057c536b386f6514dc66139e7af
박소정 <parksso0530@gmail.com>
2025-10-14 11:21:02 +0900

    agent가 사용할 수 있는 tool 명시 및 instruction 수정

--- adktest/multi-agent/agent.py
+++ adktest/multi-agent/agent.py
@@ -1,11 +1,20 @@
-import datetime
 from google.adk.agents import Agent
-from .tools import connector_tool
+from .tools import DriveAPI
+
+drive_api = DriveAPI()
 
 root_agent = Agent(
-    name="driveagent",
+    name="Google_Drive_agent",
     model="gemini-2.0-flash",
     description="You are helpful assitant",
-    tools=[connector_tool],
-    instruction="Use connector_tool to list up files in Google Drive.",
+    tools=[
+        drive_api.get_auth_error_message,
+        drive_api.list_files,
+        drive_api.download_files,
+    ],
+    instruction="""
+    You are an AI assistant with access to user's Google Drive files. 
+    Your role is to find files in Google Drive and download it in local directory. 
+    If you don't have access to user's Google Drive, you can use get_auth_error_message tool to make user login Google and get Google drive access. 
+    """,
 )
